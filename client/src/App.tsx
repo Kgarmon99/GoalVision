19312799702a45784e40dc9bdacd2e44c1ce85c1
Kahlil Garmon <119528390+Kgarmon99@users.noreply.github.com>
2025-03-09 06:34:47 +0000

    i

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,15 +33,12 @@ function Router() {
   );
 }
 
-import { ErrorBackground } from "./components/error-background";
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GoalCelebrationProvider>
         <Router />
         <Toaster />
-        <ErrorBackground />
       </GoalCelebrationProvider>
     </QueryClientProvider>
   );
